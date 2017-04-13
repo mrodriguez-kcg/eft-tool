b018263e10cf4dc7a99b21c3c7546abe94d7f7fb
mrodriguez-kcg <mark1@kochcreativegroup.com>
2017-04-13 08:39:08 -0500

    Setup Stage Part I

---
+++ dist/assets/js/src/assets/js/app.js
@@ -0,0 +1,53 @@
+$(document).foundation();
+
+//Navigation show/hide
+var scrolled;
+var ScrollTop = 0;
+var delta = 5;
+var navHeight = $('header').outerHeight();
+$(window).scroll(function(event) {
+    scrolled = !0
+});
+setInterval(function() {
+    if (scrolled) {
+        hasScrolled();
+        scrolled = !1
+    }
+}, 250);
+function hasScrolled() {
+    var st = $(this).scrollTop();//ST = curent position of scroll   NAV DOWN IS DARK
+    if (Math.abs(ScrollTop - st) <= delta)//if 0 - curent location is less than or =to 5 return
+        return;
+    if (st > ScrollTop && st > navHeight) {//if current scroll is larger than 0 and it is greater than nav height remove the class nav down and add nav up
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else if (st + $(window).height() < $(document).height()) {
+        $('header').removeClass('nav-up').addClass('nav-down');        
+        if (st == 0) {
+            $('header').removeClass('nav-down');
+        }
+    } 
+        
+    ScrollTop = st;
+    // console.log(st)
+}
+//End Navigation show/hide
+
+//Mobile Nav Script
+//Targets mobile nav toggle
+$( ".burger" ).click(function() {
+  $( this ).toggleClass("open").toggleClass("close");
+  $(".navigation-column:last-child .row").toggleClass("open");
+  $('header').toggleClass("lock");
+  console.log("nav-opened");
+});
+//Sub nav for mobile
+$(".has-sub").click(function() {
+    $(this).find(".mobile-sub-nav").addClass("sub-open").removeClass("sub-close");
+    console.log("sub-opened");
+});
+$(".mobile-nav-back").click(function(e) {
+    e.stopPropagation();
+    $(this).parent(".mobile-sub-nav.sub-open").removeClass("sub-open").addClass("sub-close");
+    console.log("sub-closed");
+});
+//End Mobile Nav Script
---
+++ gruntfile.js
@@ -0,0 +1,84 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+
+  "use strict";
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed',
+          loadPath: require('node-bourbon').includePaths
+        },
+        files: {                         // Dictionary of files
+          'dist/assets/css/app.css': 'src/assets/css/app.scss'
+        }
+      }
+    },
+
+    bake: {
+        build: {
+            options: {},
+            files: {
+              "dist/index.html": "src/index.html"
+            }
+        }
+    },
+
+    copy: {
+      main: {
+        files: [
+          {src: ['src/assets/js/*'], dest: 'dist/assets/js/'},
+          {src: ['src/assets/js/**'], dest: 'dist/assets/js/'}
+        ]
+      }
+    },
+
+    watch: { 
+      sass: {
+        files: ['src/assets/css/*.scss'],
+        tasks: ['sass'],
+        options : { nospawn : true, relative:true }
+      },
+      bake: {
+        files: ['src/**/*.html', 'src/*.html', 'includes/*.html'],
+        tasks: ['bake']
+      },
+      copy: {
+        files: ['src/assets/js/*.js', 'src/assets/js/vendor/*.js'],
+        tasks: ['copy']
+      }
+    },
+
+    touch: {
+      target: ['*.html']
+    }
+
+
+
+     
+
+  });
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bake');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-touch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+};
